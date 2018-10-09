0007ffee9cfef038eb670cc683e2580fa95f5a16
Andreas Erlandsson <aner96@gmail.com>
2018-10-09 19:05:53 +0200

    DONE DONE

--- lab3/src/lab3/BookReaderController.java
+++ lab3/src/lab3/BookReaderController.java
@@ -28,11 +28,12 @@ public class BookReaderController extends Application {
 	Button find = new Button("Find");
 	TextField text = new TextField("");
 	HBox hbox = new HBox();
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root,500,500);
-		
+		Scene scene = new Scene(root, 500, 500);
+
 		Scanner scan = new Scanner(new File("undantagsord.txt"));
 		Set<String> stopwords = new HashSet<>();
 		scan.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+");
@@ -48,75 +49,63 @@ public class BookReaderController extends Application {
 			counter.process(word);
 		}
 		s.close();
-		
-		
-		
-		
-		
-		
+		scan.close();
+
 		words = FXCollections.observableArrayList(counter.getWords());
 		listView = new ListView<>(words);
 		root.setCenter(listView);
-		
-		
-		
+
 		hbox.getChildren().addAll(alpha, freq, text, find);
+		HBox.setHgrow(text, Priority.ALWAYS);
+		root.setBottom(hbox);
+
 		freq.setOnAction(event -> {
-			words.sort(new WordCountComparator());
+			words.sort((e1, e2) -> e2.getValue() - e1.getValue());
+			listView.scrollTo(0);
 		});
 		alpha.setOnAction(event -> {
-			words.clear();
-			words.addAll(counter.getWords());
+			words.sort((e1, e2) -> e1.getKey().compareTo(e2.getKey()));
+			listView.scrollTo(0);
 		});
 		find.setOnAction(event -> {
 			search();
-		   
+
 		});
-		text.setOnAction(event ->{
+		text.setOnAction(event -> {
 			search();
 		}
-		
-				);
-		
-		HBox.setHgrow(text, Priority.ALWAYS);
-		root.setBottom(hbox);
-		
+
+		);
+
 		primaryStage.setTitle("BookReader");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
-		
+
 	}
-	public void search(){
+
+	public void search() {
 		String findWord = text.getText().trim().toLowerCase();
 		int posInList = -1;
-	    for(int i = 0; i < words.size(); i++){
-	   	if(words.get(i).getKey().equals(findWord))
-	   	{
-	   		posInList = i;
-	   		break;
-	   	}
-	    }
-	 
-	    if(posInList != -1){
-	    listView.scrollTo(posInList);
-	    listView.getSelectionModel().select(posInList);
-	    }
-	    else{
-	    	Alert alt = new Alert(Alert.AlertType.WARNING);
-	    	alt.setHeaderText("Warning");
-	    	alt.setContentText("Can not find your word, try again.");
-	    	alt.show();
-	    }
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).getKey().equals(findWord)) {
+				posInList = i;
+				break;
+			}
+		}
+
+		if (posInList != -1) {
+			listView.scrollTo(posInList);
+			listView.getSelectionModel().select(posInList);
+		} else {
+			Alert alt = new Alert(Alert.AlertType.WARNING);
+			alt.setHeaderText("Warning");
+			alt.setContentText("Can not find your word, try again.");
+			alt.show();
+		}
 	}
-	
-	
 
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	
-	
-	
 
 }
