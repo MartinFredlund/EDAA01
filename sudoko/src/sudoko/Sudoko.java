322c49cefad093f74d972dff1b6c2b2f64b23d48
Andreas Erlandsson <aner96@gmail.com>
2018-11-14 17:12:19 +0100

    Vet inte om det funkar men nu lyckades jag i alla fall pusha?

---
+++ sudoko/src/sudoko/Sudoko.java
@@ -0,0 +1,120 @@
+package sudoko;
+
+public class Sudoko {
+	private int[][] board = new int[8][8];
+	private int[][] refBoard = new int[8][8];
+	public Sudoko(int[][] board) {
+		this.board = board;
+		this.refBoard = board;
+	}
+
+	public int getValue(int x, int y) {
+		return board[x][y];
+	}
+
+	public int[][] getBoard() {
+		return board;
+	}
+
+	public void setValue(int x, int y, int value) {
+		board[x][y] = value;
+	}
+
+	private int[] next(int x, int y) {
+		if (y != 8) {
+			return new int[] { x , y + 1 };
+		} else {
+			return new int[] { x + 1, 0 };
+		}
+	}
+
+	public boolean solver() {
+		return solve(0, 0);
+	}
+
+	private boolean solve(int x, int y) {
+
+		if (x == 9) {
+			for(int i = 0; i<9 ; i++){
+				System.out.println("");
+				for(int k = 0; k<9;k++){
+					System.out.print(board[i][k]);
+				}
+			}
+			System.out.println("");
+			
+//			for (int i = 1; i < 10; i++) {
+//				if (ruleCheck(i, x, y)) {
+//					board[x][y] = i;
+//					return true;
+//				}
+//			}
+			refBoard = board;
+			return true;
+		}
+		// check om rutan är tom
+		if (refBoard[x][y] != 0) {
+			int[] newXY = next(x, y);
+			solve(newXY[0], newXY[1]);
+		}
+		// check om siffra 1-9 kan skrivas
+		if(refBoard[x][y] == 0) {
+			for (int i = 1; i < 10; i++) {
+				if (ruleCheck(i, x, y)) {
+
+					System.out.print(x + ":");
+					System.out.print(y);
+					System.out.println("  " + i);
+					board[x][y] = i;
+					int[] newXY = next(x, y);
+					solve(newXY[0], newXY[1]);
+				} else if(i == 9){
+					board[x][y] = 0;
+				}
+			}
+		}
+		
+		if(x == 0 && y == 0 && board[x][y]!=0){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean ruleCheck(int value, int x, int y) {
+		
+		// row check
+		for (int i = 0; i < 9; i++) {
+			if (board[x][i] == value) {
+				return false;
+			}
+		}
+		// column check
+		for (int i = 0; i < 9; i++) {
+			if (board[i][y] == value) {
+				return false;
+			}
+		}
+		// box check
+		int x1 = (x) % 3;
+		int y1 = (y) % 3;
+		for (int i = x + 2; i > x - 1; i--) {
+			for (int k = y + 2; k > y - 1; k--) {
+				if (board[i - x1][k - y1] == value) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// public String toString() {
+	// StringBuilder sb = new StringBuilder();
+	// for (int i = 0; i < 9; i++) {
+	// for (int k = 0; k < 9; k++) {
+	// sb.append(board[i][k] + " ");
+	// }
+	// }
+	// return sb.toString();
+	// }
+
+}
---
+++ sudoko/src/sudoko/SudokoTest.java
@@ -0,0 +1,61 @@
+package sudoko;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import sudoko.Sudoko;
+
+public class SudokoTest {
+	private int[][] myBoard;
+	private Sudoko mySudoko;
+
+	@Before
+	public void setUp() throws Exception {
+		// myIntQueue = new FifoQueue<Integer>();
+		// myStringQueue = new FifoQueue<String>();
+		myBoard = new int[8][8];
+		mySudoko = new Sudoko(myBoard);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		myBoard = null;
+		mySudoko = null;
+	}
+
+	@Test
+	public final void testEasySudoko() {
+
+		myBoard = new int[][] { 
+				{ 0, 2, 0, 4, 5, 6, 7, 8, 9 },
+				{ 4, 5, 7, 0, 8, 0, 2, 3, 6 },
+				{ 6, 8, 9, 2, 3, 7, 0, 4, 0 },
+				{ 0, 0, 5, 3, 6, 2, 9, 7, 4 }, 
+				{ 2, 7, 4, 0, 9, 0, 6, 5, 3 },
+				{ 3, 9, 6, 5, 7, 4, 8, 0, 0 },
+				{ 0, 4, 0, 6, 1, 8, 3, 9, 7 },
+				{ 7, 6, 1, 0, 4, 0, 5, 2, 8 },
+				{ 9, 3, 8, 7, 2, 5, 0, 6, 0 } };
+		mySudoko = new Sudoko(myBoard);
+		assertTrue(mySudoko.solver());
+	}
+	
+	@Test
+	public final void testHardSudoko() {
+
+		myBoard = new int[][] { 
+				{ 6, 0, 0, 1, 0, 8, 2, 0, 3 },
+				{ 0, 2, 0, 0, 4, 0, 0, 9, 0 },
+				{ 8, 0, 3, 0, 0, 5, 4, 0, 0 },
+				{ 5, 0, 4, 6, 0, 7, 0, 0, 9 }, 
+				{ 0, 3, 0, 0, 0, 0, 0, 5, 0 },
+				{ 7, 0, 0, 8, 0, 3, 1, 0, 2 },
+				{ 0, 0, 1, 7, 0, 0, 9, 0, 6 },
+				{ 0, 8, 0, 0, 3, 0, 0, 2, 0 },
+				{ 3, 0, 2, 9, 0, 4, 0, 0, 5 } };
+		mySudoko = new Sudoko(myBoard);
+		assertTrue(mySudoko.solver());
+	}
+
+}
