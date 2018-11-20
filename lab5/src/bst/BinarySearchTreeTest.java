6af123006442e400f80e51b81b2043e8f590205f
ma1233fr-s <ma1233fr-s@panter-7.e.lth.lu.se>
2018-11-20 15:59:27 +0100

    Adding Test cases (after some mixture with fukin git...)

---
+++ lab5/src/bst/BinarySearchTreeTest.java
@@ -0,0 +1,49 @@
+package bst;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import bst.BinarySearchTree;
+
+
+class BinarySearchTreeTest {
+	
+	BinarySearchTree<Integer> myBinarySearchTree;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		myBinarySearchTree = new BinarySearchTree<Integer>();
+	}
+
+
+	@Test
+	void testHeight() {
+		assertTrue(myBinarySearchTree.add(3));
+		assertTrue(myBinarySearchTree.add(20));
+		assertTrue(myBinarySearchTree.add(34));
+		assertTrue(myBinarySearchTree.add(15));
+		assertTrue(myBinarySearchTree.height() == 3);
+	}
+	
+	@Test
+	void testAdd() {
+		assertTrue(myBinarySearchTree.add(3));
+		assertTrue(myBinarySearchTree.add(21));
+		assertTrue(myBinarySearchTree.add(45));
+		assertTrue(myBinarySearchTree.add(20));
+		assertTrue(myBinarySearchTree.root.element == 3);
+		assertTrue(myBinarySearchTree.root.right.element == 21);
+		assertTrue(myBinarySearchTree.root.right.right.element == 45);
+		myBinarySearchTree.printTree();
+	}
+	
+	@Test
+	void testSize() {
+		assertTrue(myBinarySearchTree.add(3));
+		assertTrue(myBinarySearchTree.add(2));
+		assertTrue(myBinarySearchTree.add(1));
+		assertTrue(myBinarySearchTree.add(4));
+		assertTrue(myBinarySearchTree.size() == 4);
+	}
+
+}
